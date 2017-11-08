62948c3f9fbe65ea56a106db52839bbcc1d6894d
Mike Merin <mikemerinweather@gmail.com>
2017-11-07 21:50:57 -0500

    switched to sqlite DB

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171103031736) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "scores", force: :cascade do |t|
     t.string "username"
     t.float "total"
