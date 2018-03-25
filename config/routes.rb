90b8f7b18ba13b90eeabb045dca28a53af31caa7
Mike Merin <mikemerinweather@gmail.com>
2018-03-25 12:36:27 -0400

    added top individual jumps

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :scores, only: [:index, :create, :show, :top]
       get '/top', to: 'scores#top'
       get '/top_jumps', to: 'scores#top_jumps'
+      get '/top_jumps_num', to: 'scores#top_jumps_num'
       get '/top_three', to: 'scores#top_three'
       get '/top_ten', to: 'scores#top_ten'
 
