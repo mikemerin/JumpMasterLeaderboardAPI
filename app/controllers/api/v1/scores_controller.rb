74c98f6e0379ef57988bebbd5eec084b30b8c448
Mike Merin <mikemerinweather@gmail.com>
2018-07-02 02:49:38 -0400

    version update

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ScoresController < ApplicationController
 
   def version
-    render json: { "version" => "1.01", "notes" => "Complete Game! (quick bug fixes)" }
+    render json: { "version" => "1.02", "notes" => "Complete Game! (quick bug fixes)" }
   end
 
   def index
