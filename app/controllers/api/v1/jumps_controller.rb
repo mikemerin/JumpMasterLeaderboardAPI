062fb93088b0e3b4c7e1f0554456db4a6bfca436
Mike Merin <mikemerinweather@gmail.com>
2018-08-02 22:47:13 -0400

    top

--- app/controllers/api/v1/jumps_controller.rb
+++ app/controllers/api/v1/jumps_controller.rb
@@ -12,53 +12,53 @@ class Api::V1::JumpsController < ApplicationController
     end
   end
 
-  # def top_jumps_with_names
-  #   # array of hashes
-  #   @array = [
-  #     "16px", "rdiagonal", "cdiagonal", "rukito",
-  #     "tas", "uvalve", "dvalve", "fang",
-  #     "invert", "rcorner", "charlie", "superf",
-  #     "dplane", "jdiamond", "cddiamond", "45",
-  #     "boxcorner", "fspike", "bvalve", "tvalve", "dcascade", "?????", "minif", "dinvert", "ljump", "dbvalve", "ldiamond"
-  #     ];
-  #
-  #   @jump_types = [];
-  #
-  #   @array.each do |jump|
-  #     ['_jumps', '_streak', '_points'].each do |type|
-  #       @jump_types << jump + type;
-  #     end
-  #   end
-  #
-  #   @jumps = {};
-  #
-  #   @jump_types.each do |type|
-  #     @jumps[type] = { 'username' => '', 'multi' => {}, 'id' => '', 'number' => 0 };
-  #   end
-  #
-  #   Jumps.all.each do |score|
-  #     @jump_types.each do |type|
-  #       if (score[type.to_sym].round(2) > @scores[type]['number']) then
-  #         @scores[type]['number'] = score[type.to_sym].round(2);
-  #         @scores[type]['username'] = score[:'username'];
-  #         @scores[type]['multi'] = {};
-  #         @scores[type]['multi'][score[:'username']] = [score[:'id']];
-  #         @scores[type]['id'] = score[:'id'];
-  #       elsif (score[type.to_sym].round(2) == @scores[type]['number']) then
-  #         if (@scores[type]['multi'][score[:'username']]) then
-  #           @scores[type]['multi'][score[:'username']] << score[:'id'];
-  #         else
-  #           @scores[type]['multi'][score[:'username']] = [score[:'id']];
-  #           @scores[type]['username'] = 'Multiple People';
-  #           @scores[type]['id'] = '';
-  #         end
-  #       end
-  #     end
-  #   end
-  #
-  #   render json: @jumps
-  #
-  # end
+  def top
+    # array of hashes
+    @array = [
+      "16px", "rdiagonal", "cdiagonal", "rukito",
+      "tas", "uvalve", "dvalve", "fang",
+      "invert", "rcorner", "charlie", "superf",
+      "dplane", "jdiamond", "cddiamond", "45",
+      "boxcorner", "fspike", "bvalve", "tvalve", "dcascade", "?????", "minif", "dinvert", "ljump", "dbvalve", "ldiamond"
+      ];
+
+    @jump_types = [];
+
+    @array.each do |jump|
+      ['_jumps', '_streak', '_points'].each do |type|
+        @jump_types << jump + type;
+      end
+    end
+
+    @jumps = {};
+
+    @jump_types.each do |type|
+      @jumps[type] = { 'username' => '', 'multi' => {}, 'id' => '', 'number' => 0 };
+    end
+
+    Jumps.all.each do |score|
+      @jump_types.each do |type|
+        if (score[type.to_sym].round(2) > @scores[type]['number']) then
+          @scores[type]['number'] = score[type.to_sym].round(2);
+          @scores[type]['username'] = score[:'username'];
+          @scores[type]['multi'] = {};
+          @scores[type]['multi'][score[:'username']] = [score[:'id']];
+          @scores[type]['id'] = score[:'id'];
+        elsif (score[type.to_sym].round(2) == @scores[type]['number']) then
+          if (@scores[type]['multi'][score[:'username']]) then
+            @scores[type]['multi'][score[:'username']] << score[:'id'];
+          else
+            @scores[type]['multi'][score[:'username']] = [score[:'id']];
+            @scores[type]['username'] = 'Multiple People';
+            @scores[type]['id'] = '';
+          end
+        end
+      end
+    end
+
+    render json: @jumps
+
+  end
 
   def show
     @jump = Jump.find(params[:id])
